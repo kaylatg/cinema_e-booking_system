7f7d7d77b7cd7b160e980d93853f54bdf8fbd45e
George Herrmann <gvhmann@gmail.com>
2023-03-21 14:56:14 -0400

    fixed promotions

--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -29,14 +29,8 @@ export const Signup = ({stateChanger}) => {
       } else if (passRef.current.value !== verPassRef.current.value) {
           document.getElementById("errorMessage").innerHTML = "Password and Verify Password must be identical";
       } else {
-        let promo = promoRef.current.value;
-        if (promo === "on") {
-          promo = "true";
-        } else {
-          promo = "false";
-        }
         register(emailRef.current.value, passRef.current.value, userRef.current.value, phoneRef.current.value, 
-          fnameRef.current.value, lnameRef.current.value, promo, streetRef.current.value,
+          fnameRef.current.value, lnameRef.current.value, promoRef.current.checked, streetRef.current.value,
           cityRef.current.value, stateRef.current.value, zipRef.current.value, countryRef.current.value).then(response =>{
           if (Number.isInteger(response.id)) {
             navigate("/login", {replace: true});
