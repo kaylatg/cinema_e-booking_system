ae72260ebb5fef3f7f5b2a298180d5582ff2ce8d
George Herrmann <gvhmann@gmail.com>
2023-04-04 12:00:03 -0400

    updated all API calls for movies/shows/theatres/showrooms

--- src/services/FromApiMovies.js
+++ src/services/FromApiMovies.js
@@ -1,24 +1,18 @@
-export async function createShow(title, trailer, description, rating, poster, actors, director, showStart, showEnd, showroomid, showroomname) {
+/* All API calls needed for Deliverable 7 */
+
+/* Creates a Movie in the database, returns the created Movie */
+export async function createMovie(title, trailer, description, rating, poster, actors, director) {
     const body = {
-        movie: {
-            title: title,
-            trailer: trailer,
-            description: description,
-            rating: rating,
-            poster: poster,
-            actors: actors,
-            director: director
-        },
-        showStart: showStart,
-        showEnd: showEnd,
-        showroom: {
-            id: showroomid,
-            name: showroomname,
-            theatreId: 1
-        }
+        title: title,
+        trailer: trailer,
+        description: description,
+        rating: rating,
+        poster: poster,
+        actors: actors,
+        director: director
     }
 
-    const response = await fetch('http://localhost:8081/api/show', {
+    const response = await fetch('http://localhost:8081/api/movie', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(body),
@@ -27,6 +21,38 @@ export async function createShow(title, trailer, description, rating, poster, ac
     return output;
 }
 
+/* Returns the Movie based on the supplied movie id */
+export async function getMovie(movieId) {
+    const response = await fetch('http://localhost:8081/api/movie/' + movieId, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+    });
+    let output = await response.json();
+    return output;
+}
+
+/* Schedules a show for a movie in a showroom at a specific time 
+   Time format: YYYY-MM-DDTHH:MM:SS. Returns the show */
+export async function scheduleShow(movieId, showroomId, starttime, endtime) {
+    const response = await fetch('http://localhost:8081/api/show', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            movie: {
+                id: movieId
+            },
+            showStart: starttime,
+            showEnd: endtime,
+            showRoom: {
+                id: showroomId
+            }
+        }),
+    });
+    let output = await response.json();
+    return output;
+}
+
+/* Returns the Show based on the supplied show id */
 export async function getShow(showId) {
     const response = await fetch('http://localhost:8081/api/show/' + showId, {
         method: 'GET',
@@ -36,6 +62,8 @@ export async function getShow(showId) {
     return output;
 }
 
+/* Returns the all shows for the Movie based on the ID and date.
+   Time format: YYYY-MM-DDTHH:MM:SS, returns any shows AFTER the supplied time */
 export async function getUpcomingShowsForMovie(movieId, date) {
     const response = await fetch('http://localhost:8081/api/movie/' + movieId + '/shows/upcoming/' + date, {
         method: 'GET',
@@ -44,3 +72,86 @@ export async function getUpcomingShowsForMovie(movieId, date) {
     let output = await response.json();
     return output;
 }
+
+/* Creates a Promotion in the database, returns the created Promotion */
+export async function createPromotion(percent, startDate, endDate, code) {
+    const response = await fetch('http://localhost:8081/api/promotion', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            discount: percent,
+            start: startDate,
+            end: endDate,
+            code: code
+        }),
+    });
+    let output = await response.json();
+    return output;
+}
+
+/* Sets up a threatre in the database, returns the created theatre */
+export async function setupTheatre() {
+    const response = await fetch('http://localhost:8081/api/theatre', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            address: {
+                street: "1234 Theatre Street",
+                state: "GA",
+                zipcode: "30605",
+                country: "USA",
+            },
+        }),
+    });
+    let output = await response.json();
+    return output;
+}
+
+/* Sets up three showrooms in the database, returns the theatre with the showrooms */
+export async function setupShowrooms(theatreId) {
+    const response = await fetch('http://localhost:8081/api/theatre/' + theatreId + '/show-room', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            theatreId: theatreId,
+            name: "Showroom 1",
+        }),
+    });
+
+    let output1 = await response.json();
+
+    const response2 = await fetch('http://localhost:8081/api/theatre/' + theatreId + '/show-room', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            theatreId: theatreId,
+            name: "Showroom 2",
+        }),
+    });
+
+    let output2 = await response.json();
+
+    const response3 = await fetch('http://localhost:8081/api/theatre/' + theatreId + '/show-room', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            theatreId: theatreId,
+            name: "Showroom 3",
+        }),
+    });
+    let output3 = await response.json();
+    
+    return getTheatre(theatreId);
+}
+
+/* Returns the theatre based on the supplied theatre id */
+export async function getTheatre(id) {
+    const response = await fetch('http://localhost:8081/api/theatre/' + id, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+    });
+    let output = await response.json();
+    return output;
+}
+
+// ONLY MISSING GET ALL MOVIES
